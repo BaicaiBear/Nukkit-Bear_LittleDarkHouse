700ecb3530beb7e92b205e6d11a1d75fdad73bb8
Bear Cabbage <xiaoxiongbaicai@outlook.com>
2018-08-20 09:26:58 +0800

    The first upload.

---
+++ src/main/java/SaveData.java
@@ -0,0 +1,8 @@
+package top.bearcabbage.littledarkhouse;
+
+import cn.nukkit.event.Listener;
+import cn.nukkit.plugin.PluginBase;
+
+public class SaveData extends PluginBase implements Listener {
+    saveResource(String filename)
+}
